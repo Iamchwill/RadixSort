cd966765262030ca5daef3c342d7ae93bc4fed6e
iamchwill <wchen20@stuy.edu>
2021-01-12 23:47:44 -0500

    Modified merge

--- Radix.java
+++ Radix.java
@@ -9,7 +9,7 @@ public class Radix{
     return out;
   }
 
-  public static void merge(MyLinkedList original, MyLinkedList[] buckets){
+  public static void merge(SortableLinkedList original, SortableLinkedList[] buckets){
     for(int i = 0; i < buckets.length; i++){
       original.extend(buckets[i]);
     }
