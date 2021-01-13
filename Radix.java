95877c68f4b2ad20d11d99fff94b4ea22fab8d56
iamchwill <wchen20@stuy.edu>
2021-01-12 20:37:54 -0500

    Made headers for all needed methods

---
+++ Radix.java
@@ -0,0 +1,21 @@
+public class Radix{
+  public static int nth(int n, int col){
+    return 0;
+  }
+
+  public static int length(int n){
+    return 0;
+  }
+
+  public static void merge(MyLinkedList original,MyLinkedList[]buckets){
+
+  }
+
+  public static void radixSortSimple(SortableLinkedList data){
+
+  }
+
+  public static void radixSort(SortableLinkedList data){
+
+  }
+}
